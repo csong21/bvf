815768e8fbc06299eec2ec5802922e0ef4e7b441
Juer Song <cecelia.song@gmail.com>
2020-08-18 09:54:17 -0600

    add loss func

--- bayesian_testing.py
+++ bayesian_testing.py
@@ -41,27 +41,67 @@ class BayesianTesting(object):
 
 	def _generate_test_statistics(
 		self, 
-		posteriors: list, 
+		posteriors: list,
+		num_groups: int 
 	) -> list:
+		for bucket, posteri in posteriors:
 
 		
 
 		return 
-			
-
+	
+	@staticmethod
 	def _calculate_expected_loss(
-        self, res_lists: list, num_groups: int
+        res_lists: list, num_groups: int
     ) -> list:
-        
-        if num_groups == 2: #ATM Only support 2 groups, need to expand
+        loss_stats = []
+        if num_groups == 2:
             a = res_lists[0]
             b = res_lists[1]
-          
-            loss_stats = []  
             loss_a = np.mean(np.maximum(b - a, 0))
             loss_b = np.mean(np.maximum(a - b, 0))
             loss_stats.append(loss_a)
             loss_stats.append(loss_b)
-        raise ValueError(f'{variant} is misspecified') 
+
+        if num_groups == 3:
+        	a = res_lists[0]
+            b = res_lists[1]
+            c = res_lists[2]
+            loss_a = np.mean(np.maximum(b - a, 0))
+            loss_a = np.mean(np.maximum(loss_a, c - a))
+            loss_b = np.mean(np.maximum(a - b, 0))
+            loss_b = np.mean(np.maximum(loss_b, c - b))
+            loss_c = np.mean(np.maximum(a - c, 0))
+            loss_b = np.mean(np.maximum(loss_c, b - c))
+            loss_stats.append(loss_a)
+            loss_stats.append(loss_b)
+            loss_stats.append(loss_c)
+
+        if num_groups == 4:
+        	a = res_lists[0]
+            b = res_lists[1]
+            c = res_lists[2]
+            d = res_lists[3]
+
+            loss_a = np.mean(np.maximum(b - a, 0))
+            loss_a = np.mean(np.maximum(loss_a, c - a))
+            loss_a = np.mean(np.maximum(loss_a, d - a))
+
+            loss_b = np.mean(np.maximum(a - b, 0))
+            loss_b = np.mean(np.maximum(loss_b, c - b))
+            loss_b = np.mean(np.maximum(loss_b, d - b))
+
+            loss_c = np.mean(np.maximum(a - c, 0))
+            loss_c = np.mean(np.maximum(loss_c, b - c))
+            loss_c = np.mean(np.maximum(loss_c, d - c))
+
+            loss_d = np.mean(np.maximum(a - d, 0))
+            loss_d = np.mean(np.maximum(loss_d, b - d))
+            loss_d = np.mean(np.maximum(loss_d, c - d))
+            
+            loss_stats.append(loss_a)
+            loss_stats.append(loss_b)
+            loss_stats.append(loss_c)
+            loss_stats.append(loss_d)
 
         return loss_stats
